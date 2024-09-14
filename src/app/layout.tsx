1f279287e2971d02080a22b588d3d9fd4d85be03
Sai <byrrajus@msu.edu>
2024-09-14 19:00:16 -0400

    Title and icon

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -4,7 +4,7 @@ import '../styles/globals.css';
 import Navbar from '../components/Navbar';
 
 export const metadata = {
-  title: 'Portfolio Website',
+  title: 'Sai Byrraju',
   description: 'My personal portfolio',
 };
 
@@ -13,7 +13,7 @@ export default function RootLayout({ children }: { children: ReactNode }) {
     <html lang="en">
       <Head>
         <link rel="preconnect" href="https://fonts.googleapis.com" />
-        <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="true" />
+        <link rel="preconnect" href="https://fonts.gstatic.com" />
         <link
           href="https://fonts.googleapis.com/css2?family=Poppins:wght@100;200;300;400;500;600;700;800;900&display=swap"
           rel="stylesheet"
