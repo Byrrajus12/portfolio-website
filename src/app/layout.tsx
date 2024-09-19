76de7effd3df88e462e5e56bc9a7b55f70d9b1db
Sai <byrrajus@msu.edu>
2024-09-19 04:37:10 -0400

    Landing Page fully functional

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,6 +3,9 @@ import Head from 'next/head';
 import '../styles/globals.css';
 import Navbar from '../components/Navbar';
 import { Cutive_Mono, Poppins, Vast_Shadow } from 'next/font/google';
+import Particles from '@/components/particles'; // Import the Particles component
+import { IconBrandLinkedin, IconBrandGithub } from '@tabler/icons-react';
+
 export const metadata = {
   title: 'Sai Byrraju',
   description: 'My personal portfolio',
@@ -36,12 +39,10 @@ export default function RootLayout({ children }: { children: ReactNode }) {
         <link rel="preconnect" href="https://fonts.googleapis.com" />
         <link rel="preconnect" href="https://fonts.gstatic.com" />
       </Head>
-      <body className="bg-black flex flex-col min-h-screen">
+      <body className="bg-black flex flex-col min-h-screen relative">
+        <Particles className="absolute inset-0 -z-10" quantity={70} staticity={60} ease={40} />
         <Navbar />
         <main className={`${poppins.variable} ${vast_shadow.variable} ${cutive_mono.variable} ` } >{children}</main>
-        <footer className="bg-black text-white text-center ">
-          <p>&copy; {new Date().getFullYear()} My Portfolio</p>
-        </footer>
       </body>
     </html>
   );
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,33 +1,45 @@
 import '../styles/globals.css';
+import { FloatingDock } from '@/components/floatingDock';
+import { IconBrandLinkedin, IconBrandGithub } from '@tabler/icons-react';
 
+const dockItems = [
+  { title: 'LinkedIn', icon: <IconBrandLinkedin />, href: 'https://www.linkedin.com/in/saibyrraju/' },
+  { title: 'GitHub', icon: <IconBrandGithub />, href: 'https://github.com/Byrrajus12' }
+];
 
 export default function HomePage() {
   return (
-  <div className="text-container">
-      <span className='poppins' style={{ color: 'white', fontSize: '40px', fontWeight: 700, wordWrap: 'break-word' }}>
-      hi<br/>
-    </span>
-    <span className='poppins' style={{ color: 'white', fontSize: '40px', fontWeight: 300, wordWrap: 'break-word' }}>
-      My name is
-    </span>
-    <span className='poppins' style={{ color: 'white', fontSize: '40px', fontWeight: 400, wordWrap: 'break-word' }}>
-      {' '}Sai<br/>
-    </span>
-    <span className='vastshadow'  style={{ color: 'white', fontSize: '40px', fontWeight: 400, wordWrap: 'break-word' }}>
-      a computer science student
-    </span>
-        <span  className='poppins'  style={{ color: 'white', fontSize: '40px', fontWeight: 300, wordWrap: 'break-word' }}>
-         {' '}
+    <div className="text-container">
+        <span className='poppins' style={{ color: 'white', fontSize: '40px', fontWeight: 700, wordWrap: 'break-word' }}>
+        hi<br/>
+      </span>
+      <span className='poppins' style={{ color: 'white', fontSize: '40px', fontWeight: 300, wordWrap: 'break-word' }}>
+        My name is
+      </span>
+      <span className='poppins' style={{ color: 'white', fontSize: '40px', fontWeight: 400, wordWrap: 'break-word' }}>
+        {' '}Sai<br/>
+      </span>
+      <span className='vastshadow'  style={{ color: 'white', fontSize: '40px', fontWeight: 400, wordWrap: 'break-word' }}>
+        a computer science student
+      </span>
+          <span  className='poppins'  style={{ color: 'white', fontSize: '40px', fontWeight: 300, wordWrap: 'break-word' }}>
+          {' '}
+          </span>
+          <span className='poppins'  style={{ color: 'white', fontSize: '40px', fontWeight: 300, wordWrap: 'break-word' }}>
+          at
         </span>
         <span className='poppins'  style={{ color: 'white', fontSize: '40px', fontWeight: 300, wordWrap: 'break-word' }}>
-        at
-       </span>
-       <span className='poppins'  style={{ color: 'white', fontSize: '40px', fontWeight: 300, wordWrap: 'break-word' }}>
-         {' '}
-     </span>
-       <span className='cutivemono' style={{ color: 'white', fontSize: '40px', fontWeight: 400, wordWrap: 'break-word' }}>
-         {'<Michigan State University/>'}
-     </span>
-  </div>
+          {' '}
+      </span>
+        <span className='cutivemono' style={{ color: 'white', fontSize: '40px', fontWeight: 400, wordWrap: 'break-word' }}>
+          {'<Michigan State University/>'}
+      </span>
+      <footer className="bg-black text-white text-center min-h-7">
+          <div className="absolute inset-x-0 bottom-0">
+            <FloatingDock items={dockItems} />
+          </div>
+        </footer>
+    </div>
+    
   );
 }
