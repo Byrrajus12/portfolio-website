8c9e05630c12c4feaa6e8feb60c289920aad9dac
Sai <byrrajus@msu.edu>
2024-09-19 15:17:12 -0400

    Revisit Text

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -9,12 +9,12 @@ export default function Navbar() {
   <nav className="bg-black text-white p-4">
       <div style={{ width: '100%', height: '100%', paddingLeft: 45, paddingRight: 20, paddingTop: 20, paddingBottom: 20, justifyContent: 'space-between', alignItems: 'center', display: 'inline-flex' }}>
         <div className="typewriter">
-          <Link href="/" className="typewriter">Sai Byrraju</Link>       
+          <Link href="/" className="typewriter" style={{fontFamily: 'Montserrat'}}>Sai Byrraju</Link>       
         </div>
         <div style={{ justifyContent: 'flex-start', alignItems: 'center', gap: 25, display: 'flex' }}>
-            <Link href="/work" className='poppins' style={{ color: 'white', fontSize: '20px', fontWeight: 300, wordWrap: 'break-word' }}>Work</Link>
-              <Link href="/projects" className='poppins' style={{ color: 'white', fontSize: '20px', fontWeight: 300, wordWrap: 'break-word' }}>Projects</Link>
-              <Link href="/resume" className='poppins' style={{ color: 'white', fontSize: '20px', fontWeight: 300, wordWrap: 'break-word' }}>Resume</Link>
+            <Link href="/work" style={{ color: 'white', fontFamily: 'Montserrat', fontSize: '20px', fontWeight: 300, wordWrap: 'break-word' }}>Work</Link>
+              <Link href="/projects" style={{ color: 'white', fontFamily: 'Montserrat', fontSize: '20px', fontWeight: 300, wordWrap: 'break-word' }}>Projects</Link>
+              <Link href="/resume" className='poppins' style={{ color: 'white', fontFamily: 'Montserrat', fontSize: '20px', fontWeight: 300, wordWrap: 'break-word' }}>Resume</Link>
               <Image src= {profilePic} width={56} height={28} alt="Picture of the author"/>
         </div>
       </div>
