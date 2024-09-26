e7ad417294e469559a13ae96285cea63b4953905
Sai <byrrajus@msu.edu>
2024-09-26 02:41:44 -0400

    Project pages updated

--- src/app/projects/[projectId]/page.tsx
+++ src/app/projects/[projectId]/page.tsx
@@ -1,14 +1,99 @@
+// "use client";
+
+// import { useParams } from 'next/navigation';
+
+// export default function ProjectPage() {
+//   const { projectId } = useParams();
+
+//   return (
+//     <section>
+//       <h2>Project {projectId}</h2>
+//       <p>This is the details page for project {projectId}.</p>
+//     </section>
+//   );
+// }
+
 "use client";
 
 import { useParams } from 'next/navigation';
+import Link from 'next/link';
+
+// Dummy projects data to fetch from
+const projects = [
+  { id: 1, title: 'Custom Python DBMS', description: 'Developed a custom Python module that emulates the behavior of Python’s built-in SQLite3 module.', technologies: ['Python', 'SQLite', 'Data Structures'], imageUrl: '/sqlitepython.png', content: [
+    'Developed a DBMS in Python that supports complex SQL operations without using SQL',
+    'Designed to ensure scalability and efficiency for processing large datasets.',
+    'Conducted testing to verify functionality, ensuring the DBMS performs for a wide range of queries and edge cases.',
+  ] },
+  { id: 2, title: 'Grocery App Development', description: 'Developed a grocery app in Android Studio.', technologies: ['Java', 'Android', 'Firebase'], imageUrl: '/blockdiagram.png', content: [
+    'Designed and implemented a user-friendly grocery management app with multi-screen navigation.',
+    'Optimized user flow through intent-based transitions, allowing for smooth navigation.'
+  ] },
+  { id: 3, title: 'Bug Squash', description: 'Designed and developed a complex Bug squash program.', technologies: ['C++', 'UI Design'], imageUrl: '/bugsquash.png', content: [
+    'Designed and developed a complex Bug Squash game from scratch, featuring a dynamic user interface that engages users in an interactive debugging experience.',
+    'Developed functionality that require players to fix code snippets to successfully squash them, enhancing problem-solving skills.'
+  ] },
+  { id: 4, title: 'saibyrraju.vercel.app', description: "My portfolio", technologies: ['Next.js', 'TypeScript', 'Tailwind CSS'], imageUrl: '/websiteimg.png', content: [
+    'Designed a responsive portfolio website to showcase my skills and projects.',
+    'Next.js for server side rendering to ensure fast loading times and better performance.',
+    'Styled with Tailwind CSS to create a modern, consistent user interface with minimal custom CSS.',
+  ] },
+];
 
 export default function ProjectPage() {
   const { projectId } = useParams();
 
+  // Convert projectId to string if it's an array
+  const id = Array.isArray(projectId) ? projectId[0] : projectId;
+
+  // Find project by id
+  const project = projects.find((proj) => proj.id === parseInt(id || '', 10));
+
+  if (!project) {
+    return <p>Project not found.</p>;
+  }
+
   return (
-    <section>
-      <h2>Project {projectId}</h2>
-      <p>This is the details page for project {projectId}.</p>
-    </section>
+    <div className="relative top-28 left-0 animate-move-up-fade-in">
+      <Link href={'/projects'}>
+        <div className="text-zinc-400 mt-1 px-16 top-5">
+          <svg className="w-6 h-6 text-zinc-500 duration-500 hover:text-white dark:hover:text-white text-center inline-flex items-center" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 14 10">
+            <path stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M13 5H1M5 9l-4-4 4-4" />
+          </svg>
+          <span className="sr-only">left arrow</span>
+        </div>
+      </Link>
+
+      <div className="mt-4 flex flex-col items-center border-black dark:border-zinc-700 rounded-lg">
+        <div className="text-center dark:text-white py-20 px-8">
+          <h2 className="text-4xl text-white font-bold">{project.title}</h2>
+          <p className="mt-5 px-16 text-zinc-300 dark:text-zinc-300">{project.description}</p>
+
+          <div className="mt-2 flex flex-wrap justify-center space-y-1">
+            {project.technologies.map((tech, index) => (
+              <p key={index} className="mt-1 items-center mx-1 px-3 py-1 text-xs text-white text-center border border-zinc-400 dark:border-white rounded-lg">
+                {tech}
+              </p>
+            ))}
+          </div>
+        </div>
+
+        <img
+          src={project.imageUrl}
+          className="border border-zinc-300 dark:border-zinc-600 rounded object-cover w-1/2"
+          alt=''
+          width="80%"
+          height="auto"
+        />
+
+        <div>
+          <ul className="py-16 px-16 text-zinc-700 dark:text-zinc-400 ">
+            {project.content.map((paragraph, index) => (
+              <li key={index} className="text-zinc-400 mb-4 list-none hover:list-disc">{paragraph}</li>
+            ))}
+          </ul>
+        </div>
+      </div>
+    </div>
   );
 }
--- src/app/work/page.tsx
+++ src/app/work/page.tsx
@@ -5,39 +5,42 @@ export default function WorkPage() {
     {
       title: 'IT Intern',
       company: 'Tech Mahindra',
+      website: 'https://www.techmahindra.com/',
       duration: 'June 2024 - August 2024',
       description: 'Worked on improving end-user performance, collaborating with deskside engineers, and providing desktop engineering support. Assisted with service management reports and coordinated meetings with key leads and managers.',
     },
     {
       title: 'Capstone App Developer',
       company: 'Whirlpool',
+      website: 'https://www.whirlpool.com/',
       duration: 'Jan 2024 - May 2024',
       description: 'Contributed to the development of a machine learning application to personalize user experiences with Whirlpool ovens, utilizing Dart, JavaScript, Plotly, MongoDB, FastAPI, and a fine-tuned BERT model.',
     },
     {
       title: 'Student Accountant',
       company: 'MSU Business and Financial Services',
+      website: 'https://sle.msu.edu/',
       duration: 'June 2022 - Jan 2023',
       description: 'Handled accounting necessities for BFS at Michigan State University.',
     },
     {
       title: 'Teaching Assistant & Tutor',
       company: 'MSU',
+      website: 'https://msu.edu/',
       duration: 'Aug 2021 - Jan 2023',
       description: 'Assisted students in courses as a Teaching assistant, and Tutor: CSE 335, CSE 232, CSE 102, ACC 230, EC 251, MTH 103.',
     },
     {
       title: 'Service Center Representative',
       company: 'MSU',
+      website: 'https://sle.msu.edu/',
       duration: 'May 2023- Present',
       description: 'Provided customer service and clerical support to students and visitors, managing inquiries.',
     },
-    // Add more experiences as needed
   ];
 
   return (
 <section className="p-8">
-      {/* <h2 className="px-6 text-3xl mt-4 text-white font-bold p-14">Work Experience</h2> */}
       <div className="px-6 pt-20 mx-auto space-y-8 max-w-8xl lg:px-8 md:space-y-16 md:pt-24 lg:pt-32">
         <div className="max-w-2xl mx-auto lg:mx-0">
           <h2 className="text-3xl font-bold tracking-tight text-zinc-100 sm:text-4xl">
@@ -54,7 +57,9 @@ export default function WorkPage() {
             className="relative bg-black p-6 border rounded-lg shadow-lg transition-transform duration-300 ease-in-out hover:translate-y-[-6px] border-zinc-600 hover:shadow-lg hover:shadow-white/30"
           >
             <h2 className="mt-4 text-3xl font text-zinc-50 group-hover:text-white sm:text-4xl font-display">{experience.title}</h2>
-            <p className="mt-4 leading-8 duration-150 text-zinc-400">{experience.company} | {experience.duration}</p>
+            <p className="mt-4 leading-8 duration-150 text-zinc-400">
+            <a href={experience.website} target="_blank" rel="noopener noreferrer" className="relative text-zinc-400 inline-block  after:bg-zinc-400 after:absolute after:h-[1px] after:w-0 after:bottom-[1px] after:left-0 hover:after:w-full after:transition-all after:duration-300 cursor-pointer"> {experience.company} </a> | {experience.duration}
+            </p>
             <p className="mt-4 leading-8 duration-150 text-zinc-400">{experience.description}</p>
           </div>
         ))}
